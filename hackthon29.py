8f430c19d93aeda9c0e6690ebd4d7121cac4b730
Baisal23 <soodaevskyba@gmail.com>
2023-05-30 17:22:33 +0600

    mashina.kg

--- hackthon29.py
+++ hackthon29.py
@@ -2,12 +2,13 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 
-URL = 'https://www.link.kg/catalog/1/'
 
+url ='https://www.mashina.kg/search/all/'
 def write_csv(data):
-    with open('link.csv', 'a') as file:
+    with open ('mashina.csv', 'a') as file:
         writer = csv.writer(file)
-        writer.writerow([data['data'],data['pric'],data['tuple']])
+        writer.writerow([data['name'],data['price'],data['description'], data['image']])
+
 
 def get_html(url):
     response = requests.get(url)
@@ -15,18 +16,16 @@ def get_html(url):
 
 def get_data(html):
     soup = BeautifulSoup(html, 'lxml')
-    list_1 = soup.find_all("tr", class_ = "r1")
-    list_2 = soup.find_all("tr", class_ = "r2")
+    mashin1 = soup.find_all("div", class_ ="list-item list-label")
 
-    for i in list_1:
-        title = i.find_all("td")
-        price = i.find("td", class_ = "tp").text
-        dict_ = {"data":title[1].text, "pric":price, "tuple":title[3].text}
-
+    for i in mashin1:
+        title = i.find("h2", class_="name").text.replace(' ', '')
+        price = i.find("p").text.replace(' ', '')
+        description = i.find("div", class_="block info-wrapper item-info-wrapper").text.replace(' ', '')
+        image = i.find('div',class_ = 'thumb-item-carousel').find('img', class_='lazy-image').get('data-src')
+        dict_ = {"name":title, "price":price, "description":description, "image":image}
+        
         write_csv(dict_)
 
 
-print(get_data(get_html(URL)))
-
-
-
+print(get_data(get_html(url)))
